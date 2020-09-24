23c9f25000ba08a117c91347330108917df667cb
Christopher Barrett <municipalshock@gmail.com>
2020-09-23 17:32:08 -0700

    update index markup

---
+++ src/components/pages/Body.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div>
+      <h3>Advanced Statistics</h3>
+      Track how your links are performing across the web with our advanced
+      statistics dashboard. Brand Recognition Boost your brand recognition with
+      each click. Generic links don't mean a thing. Branded links help instil
+      confidence in your content. Detailed Records Gain insights into who is
+      clicking your links. Knowing when and where people engage with your
+      content helps inform better decisions. Fully Customizable Improve brand
+      awareness and content discoverability through customizable links,
+      supercharging audience engagement.
+    </div>
+  );
+};
+
+export default Body;
